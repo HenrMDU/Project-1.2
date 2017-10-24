4fdf1dff0e4a68a835910eb95b6d1b8b0d1719b6
Henriette Jørgensen <31208209+HenrMDU@users.noreply.github.com>
2017-10-25 00:41:52 +0200

    Uploaded All

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+/* Slideshow
+--------------------------------------------------------------------------------------*/
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+/* Full Screen Menu
+--------------------------------------------------------------------------------------*/
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
